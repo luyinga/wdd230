c0c294964de69433d01d55515c813aa4bb87f617
luyinga <lumbaleluyinga2018@gmail.com>
2024-01-15 02:27:20 +0200

    Changes made

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+document.getElementById("lastmodified").innerHTML = "Last modification: 01/01/2024 13:00:00";
+
+let date = new Date();
+let year = Date.getFullYear();
+document.getElementById("currentYear").innerHTML = year;
