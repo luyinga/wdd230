3a8d301614063c8a2622714997ce54c38cc22078
luyinga <lumbaleluyinga2018@gmail.com>
2024-05-22 22:00:34 +0200

    Changes made

--- chamber/scripts/numVisits.js
+++ chamber/scripts/numVisits.js
@@ -8,7 +8,7 @@ let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
-	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
 }
 
 // 4️⃣ increment the number of visits by one.
